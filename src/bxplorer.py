13d2f4623251472c430690f17927dff2148c924a
yangbochang <bcyang@outlook.com>
2020-06-02 23:38:40 +0800

    check

--- src/bxplorer.py
+++ src/bxplorer.py
@@ -1,6 +1,6 @@
 # -*- coding: utf-8 -*-
 
-# Form implementation generated from reading ui file 'E:\[GitHub]\Bxplorer\src\bxplorer.ui'
+# Form implementation generated from reading ui file 'd:\Documents\[GitHub]\Bxplorer\src\bxplorer.ui'
 #
 # Created by: PyQt5 UI code generator 5.14.2
 #
@@ -14,16 +14,26 @@ class Ui_MainWindow(object):
     def setupUi(self, MainWindow):
         MainWindow.setObjectName("MainWindow")
         MainWindow.resize(800, 600)
+        font = QtGui.QFont()
+        font.setFamily("微软雅黑")
+        font.setPointSize(11)
+        MainWindow.setFont(font)
         self.centralwidget = QtWidgets.QWidget(MainWindow)
         self.centralwidget.setObjectName("centralwidget")
+        self.textBrowser = QtWidgets.QTextBrowser(self.centralwidget)
+        self.textBrowser.setGeometry(QtCore.QRect(160, 440, 371, 121))
+        self.textBrowser.setObjectName("textBrowser")
+        self.label = QtWidgets.QLabel(self.centralwidget)
+        self.label.setGeometry(QtCore.QRect(160, 405, 111, 21))
+        font = QtGui.QFont()
+        font.setFamily("微软雅黑")
+        font.setPointSize(11)
+        self.label.setFont(font)
+        self.label.setObjectName("label")
         self.pushButton = QtWidgets.QPushButton(self.centralwidget)
-        self.pushButton.setGeometry(QtCore.QRect(490, 490, 75, 23))
+        self.pushButton.setGeometry(QtCore.QRect(420, 400, 75, 31))
         self.pushButton.setObjectName("pushButton")
         MainWindow.setCentralWidget(self.centralwidget)
-        self.menubar = QtWidgets.QMenuBar(MainWindow)
-        self.menubar.setGeometry(QtCore.QRect(0, 0, 800, 23))
-        self.menubar.setObjectName("menubar")
-        MainWindow.setMenuBar(self.menubar)
         self.statusbar = QtWidgets.QStatusBar(MainWindow)
         self.statusbar.setObjectName("statusbar")
         MainWindow.setStatusBar(self.statusbar)
@@ -34,4 +44,5 @@ class Ui_MainWindow(object):
     def retranslateUi(self, MainWindow):
         _translate = QtCore.QCoreApplication.translate
         MainWindow.setWindowTitle(_translate("MainWindow", "MainWindow"))
-        self.pushButton.setText(_translate("MainWindow", "确定"))
+        self.label.setText(_translate("MainWindow", "搜索的文件夹"))
+        self.pushButton.setText(_translate("MainWindow", "Open"))
--- src/main.py
+++ src/main.py
@@ -6,13 +6,23 @@
 """
 
 import sys
-from PyQt5.QtWidgets import QApplication, QMainWindow
+from PyQt5.QtWidgets import QApplication, QMainWindow, QPushButton, QFrame
 from bxplorer import *
 
 class Bxplorer(QMainWindow, Ui_MainWindow):
     def __init__(self, parent=None):
         super(Bxplorer, self).__init__(parent)
         self.setupUi(self)
+
+    def show_ui(self, y=50):
+        self.pu = QPushButton(self)
+        self.pu.setText('sdf')
+        self.pu.setGeometry(50, y, 100, 50)
+    
+    def keyPressEvent(self, QKeyEvent):
+        self.show_ui(70)
+        self.pu.setVisible(True)
+        print('sdf')
 
 if __name__ == '__main__':
     app = QApplication(sys.argv)
